cb383782518e89ff76ffe859c3630c81981d236c
Giang Bui <buinguyentruonggiang.1112@gmail.com>
2025-04-29 09:22:31 +0700

    [BugFix]: Fix techstacks image not load

--- src/pages/TechnologiesPage.tsx
+++ src/pages/TechnologiesPage.tsx
@@ -1,68 +1,55 @@
-import { useMemo } from 'react'
-import { motion } from 'framer-motion'
+import { Fragment, useMemo } from 'react'
+import {
+  HTML5,
+  CSS3,
+  SASS,
+  JS,
+  TS,
+  ReactJS,
+  NextJS,
+  Redux,
+  Tailwind,
+  Bootstrap,
+  MUI,
+  Antd,
+  Shadcn,
+  Jira,
+  Git,
+} from '../assets/images'
 
 const TechnologiesPage = () => {
   const techStacks = useMemo(
-    () => [
-      'HTML5',
-      'CSS3',
-      'SASS',
-      'JS',
-      'TS',
-      'ReactJS',
-      'NextJS',
-      'Redux',
-      'Tailwind',
-      'Bootstrap',
-    ],
+    () => ({
+      mainTech: [HTML5, CSS3, SASS, JS, TS, ReactJS, NextJS, Redux, Tailwind, Bootstrap],
+      uiLibraries: [MUI, Antd, Shadcn],
+      tools: [Jira, Git],
+    }),
     []
   )
-
-  const uiLibraries = useMemo(() => ['MUI', 'Antd', 'Shadcn'], [])
-
-  const tools = useMemo(() => ['Jira', 'Git'], [])
 
   return (
     <div>
       <section>
         <h1 className='text-[40px] text-gray-300 font-bold'>Tech Stacks</h1>
-        <div className='grid grid-cols-5 gap-4'>
-          {techStacks.map((tech, index) => (
-            <motion.img
-              whileHover={{ scale: 1.1 }}
-              className='p-3'
-              key={index}
-              src={`src/assets/images/${tech}.svg`}
-              width={120}
-            />
+        <div className='grid grid-cols-5 max-lg:grid-cols-4 max-md:grid-cols-3 max-sm:grid-cols-2 gap-4'>
+          {techStacks.mainTech.map((tech, index) => (
+            <Fragment key={index}>{tech()}</Fragment>
           ))}
         </div>
       </section>
       <section className='mt-4'>
         <h1 className='text-[40px] text-gray-300 font-bold'>UI Library</h1>
-        <div className='grid grid-cols-5 gap-4'>
-          {uiLibraries.map((tool, index) => (
-            <motion.img
-              whileHover={{ scale: 1.1 }}
-              className='p-3'
-              key={index}
-              src={`../src/assets/images/${tool}.svg`}
-              width={120}
-            />
+        <div className='grid grid-cols-5 max-lg:grid-cols-4 max-md:grid-cols-3 max-sm:grid-cols-2 gap-4'>
+          {techStacks.uiLibraries.map((tool, index) => (
+            <Fragment key={index}>{tool()}</Fragment>
           ))}
         </div>
       </section>
       <section className='mt-4'>
         <h1 className='text-[40px] text-gray-300 font-bold'>Tools</h1>
-        <div className='grid grid-cols-5 gap-4'>
-          {tools.map((tool, index) => (
-            <motion.img
-              whileHover={{ scale: 1.1 }}
-              className='p-3'
-              key={index}
-              src={`../src/assets/images/${tool}.svg`}
-              width={120}
-            />
+        <div className='grid grid-cols-5 max-lg:grid-cols-4 max-md:grid-cols-3 max-sm:grid-cols-2 gap-4'>
+          {techStacks.tools.map((tool, index) => (
+            <Fragment key={index}>{tool()}</Fragment>
           ))}
         </div>
       </section>
